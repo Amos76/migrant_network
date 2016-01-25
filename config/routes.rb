7f2f7635a186e3a4a457e5ccf6739abf26068450
Amos Doornbos <amos@facesofanotherworld.com>
2016-01-25 17:01:32 +0000

    Added about page & team  view, action, route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get '/about' => 'pages#about'
+  get '/team' => 'pages#team'
+
   root 'pages#home'
+
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
